64c9351c8c4c1b0060c560e4d0ae5b96d850b6b5
Jarema <jaremaradom@gmail.pl>
2016-11-12 23:16:33 +0100

    c

--- Comment/views.py
+++ Comment/views.py
@@ -7,5 +7,6 @@ from .models import Utwor
 def post_list(request):
 	utwors = Utwor.objects.all()
 	return render(request, 'Comment/post_list.html', {'utwors':utwors})
-
+def logowanie(request):
+	return render(request, 'Comment/logowanie.html', {})
 # Create your views here.
