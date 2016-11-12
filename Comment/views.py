abc86bc4fed45f98253173f5b955021d96d3d7ad
Jarema <jaremaradom@gmail.pl>
2016-11-12 01:00:23 +0100

    A

--- Comment/views.py
+++ Comment/views.py
@@ -2,11 +2,10 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Comment
 from .models import Utwor
+	
 
 def post_list(request):
-	comments = Comment.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-	return render(request, 'Comment/post_list.html', {})
-def utwor_list(request):
-	utwors = Utwor.objects.filter(published_date__lte=timezone.now()).order_by('created_date')
+	utwors = Utwor.objects.all()
 	return render(request, 'Comment/post_list.html', {'utwors':utwors})
+
 # Create your views here.
